2bc63e1690d5190e6aa1486091973baab2d3bf60
Alexandra-Aguilar <88462064+Alexandra-Aguilar@users.noreply.github.com>
2021-08-10 22:24:08 -0500

    first commit

---
+++ SistemaClientes/src/utilidades/Utilidad.java
@@ -0,0 +1,15 @@
+package utilidades;
+
+public class Utilidad {
+	
+	public void limpiarPantalla() {
+		for (int i=0; i<20; i++) {
+			System.out.println(" ");
+		}
+	}
+	
+	public void printLine(String mensaje) {
+		System.out.println(mensaje);
+	}
+
+}
